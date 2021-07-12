4106fae9bfabd938a6f1695f33f70bce05d7f954
ama <57844750+petitcinnamonroll@users.noreply.github.com>
2021-07-12 14:55:43 -0400

    updated app with different screens and backend

--- lib/model.dart
+++ lib/model.dart
@@ -5,4 +5,7 @@
 //  String PSA;
 
 //  Model({this.age, this.PSA});
+
+// this below uses age and psa as factors to calculate risk
+//100 - (10 * log(0.003 * (pow((age / 10), 3) - 341.16) + (0.186 * (log(PSA + (1 / 100))) + 1.636)))
 //}
