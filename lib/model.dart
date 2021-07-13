320fb648ec7a7c27a00e212fddb4d63b9aaf2618
chechelnitskd <dchechel@macalester.edu>
2021-07-13 11:33:40 -0500

    changed text message on results_screen.dart

--- lib/model.dart
+++ lib/model.dart
@@ -1,9 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class Model {
-//   String age;
-//   String PSA;
-//   bool BMRI;
-//
-//   Model({this.age, this.PSA});
-// }
+import 'package:flutter/material.dart';
+
+//bring in ML model
